546bd28abd3f2213f8ff3e6248297fc63b4cc558
tquang1910 <tquang1910@gmail.com>
2025-06-21 19:34:10 +0900

    Update portfolio

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,32 @@
 const navbar = document.querySelector('.navbar')
 
+// Add or remove shadow class on navbar when scrolling
 window.addEventListener('scroll', () => {
-    if(this.scrollY > 0) {
+    if(window.scrollY > 0) {
         navbar.classList.add('shadow')
     } else {
         navbar.classList.remove('shadow')
     }
 })
 
-// AOS
+// Initialize AOS animation library with duration of 700ms
 AOS.init({
     duration: 700
+});
+
+
+function changeLanguage(lang) {
+    const elements = document.querySelectorAll('[data-lang-en][data-lang-kr]');
+    elements.forEach(el => {
+        if (lang === 'en') {
+            el.textContent = el.getAttribute('data-lang-en');
+        } else if (lang === 'kr') {
+            el.textContent = el.getAttribute('data-lang-kr');
+        }
+    });
+}
+
+// Default English
+window.addEventListener('DOMContentLoaded', () => {
+    changeLanguage('en');
 });
